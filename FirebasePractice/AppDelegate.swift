9ed37bdb3de17412e533035d6232f106c2de7b99
市川龍星 <[02262ne1@gmail.com]>
2018-08-27 12:24:34 +0900

    新規作成２

--- FirebasePractice/AppDelegate.swift
+++ FirebasePractice/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
+        
         return true
     }
 
--- FirebasePractice/SignupViewController.swift
+++ FirebasePractice/SignupViewController.swift
@@ -7,21 +7,85 @@
 //
 
 import UIKit
+import Firebase
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordTextField.isSecureTextEntry = true
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func willSignup(_ sender: Any) {
+        signUp()
+    }
+    
+    
+    @IBAction func willTransitionToLogin(_ sender: Any) {
+        transitionToLogin()
+    }
+    
 
+    func transitionToLogin() {
+        self.performSegue(withIdentifier: "toLogin", sender: self)
+    }
+    
+    func transitionToView() {
+        self.performSegue(withIdentifier: "toView", sender: self)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func signUp() {
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {(user, error) in
+            if error == nil {
+                self.transitionToLogin()
+                
+            };)
+        }, else {
+            print("\(error?.localizedDescription)")
+        }
+    
+    }
+    
+//    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+//    //エラーなしなら、認証完了
+//    if error == nil{
+//    // エラーがない場合にはそのままログイン画面に飛び、ログインしてもらう
+//    self.transitionToLogin()
+//    })
+//    }else {
+//
+//    print("\(error?.localizedDescription)")
+//    }
+//    })
+//}
     /*
     // MARK: - Navigation
 
